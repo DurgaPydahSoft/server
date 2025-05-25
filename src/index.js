188e0fd44ece7b67f56a9fc4d87bb4aae5701866
DurgaPydahSoft <durgaprasad@pydahsoft.in>
2025-05-25 14:04:39 +0530

    Update CORS middleware to allow PATCH method in Express app

--- src/index.js
+++ src/index.js
@@ -47,7 +47,7 @@ app.use(cors({
     "http://localhost:5173"
   ],
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
 app.use(express.json());
