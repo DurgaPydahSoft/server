e3b0e72ec6e4e0d3959b5534149707275f7fb972
DurgaPydahSoft <durgaprasad@pydahsoft.in>
2025-08-19 20:49:25 +0530

    Removed unused files

--- app.js
+++
@@ -1,9 +0,0 @@
-const pushSubscriptionRoutes = require('./routes/pushSubscriptions');
-
-// Routes
-app.use('/api/auth', authRoutes);
-app.use('/api/users', userRoutes);
-app.use('/api/complaints', complaintRoutes);
-app.use('/api/announcements', announcementRoutes);
-app.use('/api/polls', pollRoutes);
-app.use('/api/push-subscriptions', pushSubscriptionRoutes); 
